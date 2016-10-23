f1eee79798860cbfc782df38f9deb9b5c0ce6c08
Radek Los <radek.los@gmail.com>
2016-10-23 19:30:31 +0200

    CORS configuration

---
+++ src/main/java/com/caribou/auth/AppConfiguration.java
@@ -0,0 +1,27 @@
+package com.caribou.auth;
+
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration localhost = new CorsConfiguration();
+        localhost.setAllowCredentials(true);
+        localhost.addAllowedOrigin("http://localhost:8000");
+        localhost.addAllowedHeader("*");
+        localhost.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", localhost);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
