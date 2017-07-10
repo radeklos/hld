fedf3ddf5cc55591adbfb3f6374e195eb4eec805
Radek Los <radek.los@gmail.com>
2017-07-11 00:25:52 +0200

    Calculate number of time offs in days

--- src/main/java/com/caribou/holiday/service/LeaveService.java
+++ src/main/java/com/caribou/holiday/service/LeaveService.java
@@ -4,7 +4,9 @@ import com.caribou.auth.domain.UserAccount;
 import com.caribou.company.domain.CompanyEmployee;
 import com.caribou.company.repository.CompanyRepository;
 import com.caribou.company.service.RxService;
+import com.caribou.holiday.domain.BankHoliday;
 import com.caribou.holiday.domain.Leave;
+import com.caribou.holiday.repository.BankHolidayRepository;
 import com.caribou.holiday.repository.LeaveRepository;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +14,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rx.Observable;
 
+import java.math.BigDecimal;
+import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -27,6 +37,9 @@ public class LeaveService extends RxService.Imp<LeaveRepository, Leave, UUID> {
 
     @Autowired
     private CompanyRepository companyRepository;
+
+    @Autowired
+    private BankHolidayRepository bankHolidayRepository;
 
     public Observable<Leave> findByUserAccount(UserAccount userAccount) {
         return Observable.from(leaveRepository.findByUserAccount(userAccount));
@@ -44,6 +57,34 @@ public class LeaveService extends RxService.Imp<LeaveRepository, Leave, UUID> {
                         .build()
                 )
                 .collect(Collectors.toList());
+    }
+
+    public BigDecimal numberOfBookedDays(Leave leave, BankHoliday.Country country) {
+        LocalDateTime starting = leave.getStarting().toLocalDateTime();
+        LocalDateTime ending = leave.getEnding().toLocalDateTime();
+
+        BigDecimal days = BigDecimal.ZERO;
+        for (LocalDateTime day = leave.getStarting().toLocalDateTime();
+             day.isBefore(leave.getEnding().toLocalDateTime());
+             day = day.toLocalDate().atStartOfDay().plusDays(1)
+                ) {
+            LocalDate asLocalDate = day.toLocalDate();
+            if (isWeekend(asLocalDate) || bankHolidayRepository.isBankHoliday(Date.valueOf(asLocalDate), country)) {
+                continue;
+            }
+            LocalDateTime end = asLocalDate.atTime(LocalTime.MAX);
+            if (leave.getEnding().toLocalDateTime().isBefore(end)) {
+                end = leave.getEnding().toLocalDateTime();
+            }
+            Duration duration = Duration.between(day, end);
+            days = days.add(BigDecimal.valueOf(duration.toHours()).divide(BigDecimal.valueOf(24), 1, BigDecimal.ROUND_HALF_UP));
+        }
+        return days;
+    }
+
+    private boolean isWeekend(LocalDate localDate) {
+        Set<DayOfWeek> weekend = EnumSet.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+        return weekend.contains(localDate.getDayOfWeek());
     }
 
     @Data
--- src/test/java/com/caribou/holiday/service/LeaveServiceTest.java
+++ src/test/java/com/caribou/holiday/service/LeaveServiceTest.java
@@ -8,15 +8,22 @@ import com.caribou.company.domain.Company;
 import com.caribou.company.domain.Role;
 import com.caribou.company.repository.CompanyRepository;
 import com.caribou.company.service.CompanyService;
+import com.caribou.holiday.domain.BankHoliday;
 import com.caribou.holiday.domain.Leave;
 import com.caribou.holiday.domain.LeaveType;
+import com.caribou.holiday.repository.BankHolidayRepository;
 import com.caribou.holiday.repository.LeaveTypeRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import rx.observers.TestSubscriber;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,6 +51,9 @@ public class LeaveServiceTest extends IntegrationTests {
 
     @Autowired
     private LeaveService leaveService;
+
+    @Autowired
+    BankHolidayRepository bankHolidayRepository;
 
     @Before
     public void setUp() throws Exception {
@@ -103,6 +113,103 @@ public class LeaveServiceTest extends IntegrationTests {
         List<LeaveService.EmployeeLeaves> leaves = leaveService.getEmployeeLeaves(company.getUid().toString(), from, to);
 
         assertThat(leaves).hasSize(2);
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysWithHolidays() throws Exception {
+        Date date = Date.valueOf(LocalDate.of(2017, 6, 13));
+        BankHoliday bankHoliday = BankHoliday.builder()
+                .country(BankHoliday.Country.CZ)
+                .date(date)
+                .build();
+        bankHolidayRepository.save(bankHoliday);
+
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDate.of(2017, 6, 12).atStartOfDay()))
+                .ending(Timestamp.valueOf(LocalDate.of(2017, 6, 14).atTime(LocalTime.MAX)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(2));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysDuringWeekend() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDate.of(2017, 7, 7).atStartOfDay()))
+                .ending(Timestamp.valueOf(LocalDate.of(2017, 7, 12).atTime(LocalTime.MAX)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(4));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysForWholeWeek() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDate.of(2017, 7, 10).atStartOfDay()))
+                .ending(Timestamp.valueOf(LocalDate.of(2017, 7, 16).atTime(LocalTime.MAX)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(5));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysForOneDay() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDate.of(2017, 7, 3).atStartOfDay()))
+                .ending(Timestamp.valueOf(LocalDate.of(2017, 7, 3).atTime(LocalTime.MAX)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(1));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysForTheFirstHalfOfDay() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDate.of(2017, 7, 3).atStartOfDay()))
+                .ending(Timestamp.valueOf(LocalDateTime.of(2017, 7, 3, 12, 0)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(0.5));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysForTheSecondHalfOfDay() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDateTime.of(2017, 7, 3, 12, 0)))
+                .ending(Timestamp.valueOf(LocalDate.of(2017, 7, 3).atTime(LocalTime.MAX)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(0.5));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysForHalfOfDayDuringHoliday() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDateTime.of(2017, 7, 15, 12, 0)))
+                .ending(Timestamp.valueOf(LocalDate.of(2017, 7, 15).atTime(LocalTime.MAX)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(0));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysForHalfOfDaysDuringWeekend() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDateTime.of(2017, 7, 13, 12, 0)))
+                .ending(Timestamp.valueOf(LocalDateTime.of(2017, 7, 18, 12, 0)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(3));
+    }
+
+    @Test
+    public void calculateNumberOfBookedDaysForHalfOfDaysDuringWeekend2() throws Exception {
+        Leave leave = Leave.builder()
+                .starting(Timestamp.valueOf(LocalDateTime.of(2017, 7, 14, 12, 0)))
+                .ending(Timestamp.valueOf(LocalDateTime.of(2017, 7, 16, 12, 0)))
+                .build();
+        BigDecimal bookedDaysOff = leaveService.numberOfBookedDays(leave, BankHoliday.Country.CZ);
+        assertThat(bookedDaysOff).isEqualByComparingTo(BigDecimal.valueOf(0.5));
     }
 
 }
